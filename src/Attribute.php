42ce49fa83b20467b0a2069704042b319b5c396c
Mr.Professor <mronx@ya.ru>
2022-10-27 15:16:57 +0500

    Add attribute instance to attribute dto class.

--- src/Attribute.php
+++ src/Attribute.php
@@ -13,18 +13,20 @@ use ReflectionProperty;
 class Attribute
 {
     public string $target = '';
+    public object|null $instance = null;
 
     /**
      * @throws \Exception
      */
     public function __construct(
-        public readonly ReflectionAttribute $attribute,
-        public readonly ?ReflectionClass $class = null,
-        public readonly ?ReflectionMethod $method = null,
-        public readonly ?ReflectionProperty $property = null,
+        public readonly ReflectionAttribute      $attribute,
+        public readonly ?ReflectionClass         $class = null,
+        public readonly ?ReflectionMethod        $method = null,
+        public readonly ?ReflectionProperty      $property = null,
         public readonly ?ReflectionClassConstant $constant = null,
-        public readonly ?ReflectionParameter $parameter = null,
-    ) {
+        public readonly ?ReflectionParameter     $parameter = null,
+    )
+    {
         $this->target = match ($this->attribute->getTarget()) {
             \Attribute::TARGET_CLASS => 'class',
             \Attribute::TARGET_METHOD => 'method',
@@ -34,6 +36,7 @@ class Attribute
             \Attribute::TARGET_ALL => 'all',
             default => throw new Exception('Unknown target'),
         };
+        $this->instance = $this->attribute->newInstance();
     }
 
     public function toArray(): array
@@ -47,6 +50,7 @@ class Attribute
             'target' => $this->target,
             'name' => $this->attribute->getName(),
             'arguments' => $this->attribute->getArguments(),
+            'instance' => (array)($this->instance) ?? null,
         ];
 
         return config('attribute-scanner.filtered_to_array') === true ? array_filter($arr) : $arr;
